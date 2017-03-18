21e65da7c0c19b4ea4963534ed9bd26dcf1cf6a3
Christopher Hrabia <c.hrabia@gmail.com>
2017-03-18 13:46:34 +0100

    Added script for disabling gphoto2 gvfs monitor

---
+++ disable_gphoto2_gvfs.sh
@@ -0,0 +1,3 @@
+
+sudo mv /usr/share/dbus1/services/org.gtk.Private.GPhoto2VolumeMonitor.service /usr/share/dbus1/services/org.gtk.Private.GPhoto2VolumeMonitor.service.bak
+sudo mv /usr/share/gvfs/remote-volume-monitors/gphoto2.monitor /usr/share/gvfs/remote-volume-monitors/gphoto2.monitor.bak
