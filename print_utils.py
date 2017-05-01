f29d685b39b5823bffa2b7028c9a6f8d17ff6a3f
hrabia <hrabia@toffer>
2017-05-01 11:35:55 +0200

    starting, stoping and availability functions for printer

--- print_utils.py
+++ print_utils.py
@@ -1,15 +1,46 @@
 
 from smb.SMBConnection import SMBConnection
 import os
+import subprocess
+from wakeonlan import wol
+import shlex
 
 SMB_USER = 'pi'
 SMB_PW = 'Test1234'
 SERVER_NAME = 'pi-printer'
 SMB_SHARE = 'photos'
 SERVER_IP = '192.168.10.2'
+SERVER_MAC = 'TODO'
 CLIENT_NAME = 'raspberry'
 
-def print_photo( photo_file):
+
+def start_printer():
+    wol.send_magic_packet(SERVER_MAC)
+
+def stop_printer():
+    "net rpc shutdown -I 192.168.10.2 -U pi%Test1234 -f"
+
+    command = "net rpc shutdown -I {0} -U {1}%{2} -f".format(SERVER_IP, SMB_USER, SMB_PW )
+    args = shlex.split(command)
+    subprocess.check_call(args)
+
+def printer_available():
+
+    try:
+        conn = SMBConnection(SMB_USER, SMB_PW,
+                             CLIENT_NAME, SERVER_NAME,
+                             use_ntlm_v2=False)
+        if conn.connect(SERVER_IP, 139, timeout = 2):
+            conn.close()
+            return True
+        else:
+            return False
+    except:
+        return False
+
+    return True
+
+def print_photo(photo_file):
     """
     printing a photo on a remote printer
     In this case we copy the file with smb to a remote windows server that monitors that share and prints the files
