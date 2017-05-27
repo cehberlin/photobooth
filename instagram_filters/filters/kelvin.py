8ff3cb2f0ea0d0f33506ebf32006cc4790f3a1ef
hrabia <hrabia@toffer>
2017-05-27 14:45:06 +0200

    fixed kelvin filter

--- instagram_filters/filters/kelvin.py
+++ instagram_filters/filters/kelvin.py
@@ -4,5 +4,5 @@ from instagram_filters.decorations import Frame
 class Kelvin(Filter, Frame):
 	
 	def apply(self):
-		self.execute("convert \( {0} -auto-gamma -modulate 120,50,100 \) \( -size {width}x{height} -fill 'rgba(255,153,0,0.5)' -draw 'rectangle 0,0 {width},{height}' \) -compose multiply {0}".format(r'"{filename}"'));
+		self.execute("convert \( \"{filename}\" -auto-gamma -modulate 120,50,100 \) \( -size {width}x{height} -fill 'rgba(255,153,0,0.5)' -draw 'rectangle 0,0 {width},{height}' \) -compose multiply \"{filename}\"")
 		#self.frame("Kelvin.jpg");
