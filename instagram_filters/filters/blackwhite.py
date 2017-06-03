475c95d35e89c69c58cbd3ee6a390e7c64167684
hrabia <hrabia@toffer>
2017-06-03 16:38:47 +0200

    improved b&w filter

--- instagram_filters/filters/blackwhite.py
+++ instagram_filters/filters/blackwhite.py
@@ -3,4 +3,4 @@ from instagram_filters.filter import Filter
 class BlackAndWhite(Filter):
 	
 	def _filter_callback(self):
-		self.add_filter_step(process_step_cmd="-separate -poly '0.25,1, 0.5,1, 0.25,1' -level 0%,100%,0.8")
+		self.add_filter_step(process_step_cmd="-separate -poly '0.25,1, 0.5,1, 0.25,1' -sigmoidal-contrast 5,50%")
