0b7d4436d867b0f57b0f92d62931cc867c322f57
hrabia <c.hrabia@gmail.com>
2017-07-23 13:56:47 +0200

    Added gphoto commandline interface GPhotoCMDCamera

--- camera.py
+++ camera.py
@@ -2,11 +2,15 @@ import piggyphoto.piggyphoto as piggyphoto
 import pygame
 import imp
 
+import subprocess
+import shlex
+
 from utils import GenericClassFactory
 from shutil import copy
 from datetime import datetime
 
 from abc import ABCMeta, abstractmethod
+from subprocess import Popen, PIPE
 
 
 class AbstractCamera(object):
@@ -272,14 +276,156 @@ class DummyCamera(AbstractCamera):
 
 camera_factory.register_algorithm("dummy", DummyCamera)
 
+class GPhotoCMDCamera(AbstractCamera):
+    """
+    Class wrapping camera access through piggyphoto gphoto2 library for Nikon DSLR, you probably have to adjust it
+    for other camera brands
+    """
+
+    def __init__(self,  photo_directory, tmp_directory, **kwargs):
+        """
+        :param photobooth: app instance
+        :param kwargs:
+        """
+        self._photo_directory = photo_directory
+        self._tmp_directory = tmp_directory
+        self._shell_p = None
+
+    def set_memory_capture(self):
+        # set capturetarget to memory card
+        self._set_config('/main/settings/capturetarget', 'Memory card')
+
+    def set_idle(self):
+        self._disable_shell()
+
+    def disable_liveview(self):
+        self._set_config('/main/actions/viewfinder', 0)
+
+    def enable_liveview(self):
+        self._set_config('/main/actions/viewfinder', 1)
+
+    def get_preview(self):
+        """
+        get a camera preview
+        :return: pygame image
+        """
+        preview_picture = None
+        try:
+            if not self._shell_p:
+                self._enable_shell()
+
+            command = "capture-preview "
+            self._input_shell(command)
+            file = 'capture_preview.jpg'
+            preview_picture = pygame.image.load(file)
+        except:
+            pass
+
+        return preview_picture
+
+    def enable_live_autofocus(self):
+        self._set_config('/main/capturesettings/liveviewaffocus', 'Full-time-servo AF')
+
+    def disable_live_autofocus(self):
+        self._set_config('/main/capturesettings/liveviewaffocus', 'Single-servo AF')
+
+    def take_photo(self):
+        """
+        Trigger photo capture
+        :return:  tuple of pygame image and path to file
+        """
+        # disable liveview to use better internal autofocus of camera by disabling shell mode
+        self._disable_shell()
+
+        file = self._photo_directory + "/dsc_" + str(datetime.now()).replace(':','-') + ".jpg"
+        command = "gphoto2  --capture-image-and-download --filename {filename}".format(filename=r'"%s"' % file)
+        self._execute_process(command)
+
+        return pygame.image.load(file), file
+
+    def _set_config(self, key, value):
+        """
+        set a gphoto2 config value
+        function automatically handles non-shell and shell mode
+        :param key: config key
+        :param value: config value
+        """
+
+        if not self._shell_p:
+            if isinstance(value, basestring):
+                command = "gphoto2  --set-config {key}={value}".format(key=key, value=r'"%s"' % value)
+            else:
+                command = "gphoto2  --set-config {key}={value}".format(key=key, value=value)
+            self._execute_process(command)
+        else:
+            if isinstance(value, basestring):
+                command = "set-config {key}={value}".format(key=key, value=value)
+            else:
+                command = "set-config {key}={value}".format(key=key, value=value)
+
+            self._input_shell(command)
+
+    def _execute_process(self, command):
+        """
+        Execute a process command
+        :param command: full command string
+        """
+        cmd = shlex.split(command)
+
+        p = subprocess.Popen(cmd, shell=False, stderr=subprocess.STDOUT)
+        p.wait()
+
+    def _enable_shell(self):
+        """
+        enable gphoto2 interactive shell mode
+        """
+        command = 'gphoto2 --shell --force-overwrite'
+
+        cmd = shlex.split(command)
+
+        if self._shell_p:
+            self._disable_shell()
+
+        self._shell_p = subprocess.Popen(cmd, stdin=PIPE, stdout=PIPE)
+
+    def _disable_shell(self):
+        """
+        disable gphoto2 interactive shell mode
+        check if it was started at all
+        """
+        if self._shell_p:
+            self._shell_p.stdin.write('exit\n')
+            self._shell_p = None
+
+    def _input_shell(self, cmd):
+        """
+        send a command to a running interactive gphoto shell
+        :param cmd: the gphoto2 command
+        """
+        if self._shell_p:
+            self._shell_p.stdin.write(cmd + '\n')
+        else:
+            raise Exception("Gphoto2 shell mode was not started")
+
+    def close(self):
+        if self._shell_p:
+            self.set_idle()
+
+    def __del__(self):
+        self.close()
+
+camera_factory.register_algorithm("gphotocmd", GPhotoCMDCamera)
+
 if __name__ == '__main__':
 
-    cam = GPhoto2CffiCamera('images', 'tmp')
+    cam = GPhotoCMDCamera('images', 'tmp')
     #cam = PiggyphotoCamera('images', 'tmp')
+    preview = cam.get_preview()
+    cam.enable_live_autofocus()
 
     cnt = 0
     while True:
-        photo = cam.take_photo()
+        #photo = cam.take_photo()
         preview = cam.get_preview()
         cnt +=1
-	print(cnt)
+        #print(cnt)
