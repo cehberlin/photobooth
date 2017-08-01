5f149271b6357d2121d11a3428987fc5809bc416
Christopher Hrabia <c.hrabia@gmail.com>
2017-08-01 13:57:58 +0200

    further extended filter test script

--- test_filter.py
+++ test_filter.py
@@ -1,12 +1,16 @@
-from instagram_filters.filters import Nashville
+from instagram_filters.filters import Nashville, Toaster, BlackAndWhite
 from timeit import default_timer as timer
 
 start = timer()
 
-filter = Nashville(filename='tmp/dummy_snap.jpg', output_filename='/tmp/dummy_snap_filtered.jpg')
+filter = Nashville(filename='tmp/dummy_snap.jpg', output_filename='/tmp/dummy_snap_nashville_filtered.jpg')
 # this is for comparision with the original  instagram_filters library
 #filter = Nashville(filename='dsc_2017.jpg')
-
 filter.apply()
+filter = Toaster(filename='tmp/dummy_snap.jpg', output_filename='/tmp/dummy_snap_toaster_filtered.jpg')
+filter.apply()
+filter = BlackAndWhite(filename='tmp/dummy_snap.jpg', output_filename='/tmp/dummy_snap_bw_filtered.jpg')
+filter.apply()
+
 end = timer()
 print(end - start)
