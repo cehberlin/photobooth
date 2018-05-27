137e1880f583fe79ba424aaf4fe2a30787b607f5
hrabia <c.hrabia@gmail.com>
2018-05-27 21:21:53 +0200

    missing logo decoration filter

---
+++ instagram_filters/decorations/logo.py
@@ -0,0 +1,15 @@
+from decoration import Decoration
+
+class Logo(Decoration):
+
+    def __init__(self, logo_path):
+        self.logo_path = logo_path
+
+    def apply(self, filter):
+
+        filter.add_filter_step(
+            process_step_cmd="'{logo}' -gravity NorthEast -geometry +25+25 -composite",
+            logo=self.logo_path
+        )
+
+
